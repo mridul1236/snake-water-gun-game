f47e0fec83e8e3c3d926acdfae64112e0c716d4e
Mridul <MRIDULMAHAJAN1236@GMAIL.COM>
2025-07-12 23:00:23 +0530

    Add files via upload

---
+++ mainshort.py
@@ -0,0 +1,39 @@
+import random
+'''
+1 for snake 
+-1 for water
+0 for gun
+'''
+computer = random.choice([1,-1,0])
+yourchoise = input ("Enter your choise :")
+youdict = { "s" : 1 , "w" : -1 , "g" : 0}
+reversedict = { 1 : "s" ,  -1 : "w" , 0 : "g"}
+
+you = youdict[yourchoise]
+ 
+print(f"you chose {reversedict[you]}\ncomputer chose {reversedict[computer]}")
+
+if (computer == you):
+    print("it's a draw")
+# else:
+#     if(computer == 1 and you == -1): # 1-(-1) = 2
+#         print(" you lose!") 
+#     elif(computer == 1 and you == 0): # 1 - 0 = 1
+#         print(" you win!") 
+#     elif(computer == -1 and you == 1):# -1 - (-1) = -2
+#         print(" you win!") 
+#     elif(computer == -1 and you == 0):# -1 -0 = -1
+#         print(" you lose!") 
+#     elif(computer == 0 and you ==  1):# 0 - 1 = -1
+#         print(" you lose!") 
+#     elif(computer == 0 and you == -1): # 0 -(-1) = 1
+#         print(" you win!") 
+#     else:
+#         print("something went wrong")
+
+# a simple pattern is obsserved that when we get computer - you = 2 or -1 then you lose else you win thus
+else: 
+    if((computer -  you) == 2 or (computer -  you) == -1):
+        print("you lose!")
+    else:
+        print("you won!")
